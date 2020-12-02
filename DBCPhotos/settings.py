5167356315eda81bad42c30badb974803aa65704
smlethil <40178928+Suanlianmang@users.noreply.github.com>
2020-12-02 10:12:48 +0530

    Update settings.py

--- DBCPhotos/settings.py
+++ DBCPhotos/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
